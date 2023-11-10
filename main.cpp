0506bc96ad5edfd3a525492c325aa8b42f796fe3
olexijklimov <klim.leha.36@gmail.com>
2023-11-10 22:41:52 +0200

    v2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iosteam>
+#include <cstdlib>
 using namespace std;
 int main()
 {
-    int i;
-    for (i=0; i<5; i++)
+    int i, a;
+    cout<<"a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+    c=a+rand%(b-a+1);
+    for (i=0; i<c; i++)
     {
         cout<<"Hello world!";
     }
---
+++ second.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, c;
+    cout<<" a"<<" b";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"c="<<c;
+}
